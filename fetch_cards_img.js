da75d613fe7975472726f9e792e7348b49aa5089
willzhao <gnomeek@outlook.com>
2024-10-25 15:20:55 +0800

    fetch resources from netease

---
+++ fetch_cards_img.js
@@ -0,0 +1,34 @@
+// https://marvelsnap.163.com/zlk/
+let imageInfo = [];
+function simulateClick(dataNum) {
+  console.log(`模拟点击 data-num="${dataNum}"`);
+  document.querySelector(`li[data-num="${dataNum}"]`).click();
+  const movieElements = document.querySelectorAll('ul.list.card_list.on li.movie');
+  const imgInfoArray = Array.from(movieElements).map(li => {
+    const img = li.querySelector('img');
+    if (img) {
+      const src = img.getAttribute('src');
+      return {
+        name: img.src.split('/').pop(),
+        src: src,
+        pool: dataNum
+      };
+    }
+    return null;
+  }).filter(Boolean);
+  imageInfo.push(...imgInfoArray)
+}
+
+for (let i = 0; i <= 7; i++) {
+  simulateClick(i.toString());
+}
+
+let jsonString = JSON.stringify(imageInfo, null, 2);
+let blob = new Blob([jsonString], {type: "application/json"});
+let a = document.createElement('a');
+a.href = URL.createObjectURL(blob);
+a.download = 'card_images_info.json';
+a.textContent = 'download json';
+a.style.display = 'block';
+document.body.appendChild(a);
+console.log('click the link to download json');
