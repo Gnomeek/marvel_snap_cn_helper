78f0b9731baf0e85eb38136cc1afc1c4ea3b5cb6
Gnomeek <gnomeek@outlook.com>
2024-11-04 17:47:58 +0800

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import RootLayout from "@/components/DesktopLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
       <Component {...pageProps} />
+      <Analytics />
     </RootLayout>
   );
 }
