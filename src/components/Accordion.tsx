ec7f82ba56d54e765331417d59dd28eb79c4c4f5
Gnomeek <gnomeek@outlook.com>
2024-11-18 15:47:17 +0800

    show cnt

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,7 +1,7 @@
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Grid from "@mui/material/Grid2";
 import Card, { CardProps } from "./Card";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useShallow } from "zustand/react/shallow";
 import { CardStateEnum, useCardState } from "@/hooks/cards";
 import {
@@ -27,7 +27,13 @@ export default function Accordion({
   cards,
   defaultOpen = true,
 }: AccordionProps) {
+  const [isClient, setIsClient] = useState(false);
+
   const [isOpen, setIsOpen] = useState(defaultOpen);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const { setObtained, cardStates, selectedCardState } = useCardState(
     useShallow((state) => ({
@@ -48,6 +54,33 @@ export default function Accordion({
     []
   );
 
+  const obtainedCount = useMemo(() => {
+    if (!isClient) return 0;
+    let cnt = 0;
+    cardStates.forEach((v, k) => {
+      if (
+        v === CardStateEnum.OBTAINED &&
+        cards.map((c) => c.name).includes(k)
+      ) {
+        cnt++;
+      }
+    });
+    return cnt;
+  }, [cardStates, cards, isClient]);
+  const variantCount = useMemo(() => {
+    if (!isClient) return 0;
+    let cnt = 0;
+    cardStates.forEach((v, k) => {
+      if (
+        v === CardStateEnum.VARIANT_ONLY &&
+        cards.map((c) => c.name).includes(k)
+      ) {
+        cnt++;
+      }
+    });
+    return cnt;
+  }, [cardStates, cards, isClient]);
+
   return (
     <MuiAccordion
       expanded={isOpen}
@@ -62,12 +95,18 @@ export default function Accordion({
       >
         <Stack direction="row" spacing={2} className="control-buttons">
           <Typography fontSize={"24px"}>{title}</Typography>
+          <Typography fontSize={"24px"}>{`${obtainedCount}/${variantCount}/${
+            cards.length - obtainedCount - variantCount
+          }`}</Typography>
           <Button
             variant="contained"
             size="small"
             onClick={(e) => {
               e.stopPropagation();
-              setObtained(cards.map((card) => card.name), CardStateEnum.OBTAINED);
+              setObtained(
+                cards.map((card) => card.name),
+                CardStateEnum.OBTAINED
+              );
               setIsOpen(true);
             }}
           >
@@ -78,7 +117,10 @@ export default function Accordion({
             size="small"
             onClick={(e) => {
               e.stopPropagation();
-              setObtained(cards.map((card) => card.name), CardStateEnum.NOT_OBTAINED);
+              setObtained(
+                cards.map((card) => card.name),
+                CardStateEnum.NOT_OBTAINED
+              );
               setIsOpen(true);
             }}
           >
@@ -89,7 +131,11 @@ export default function Accordion({
       <AccordionDetails>
         <Grid container spacing={1}>
           {cards.map((card, index) => {
-            const status = getOrDefault(cardStates, card.name, CardStateEnum.NOT_OBTAINED);
+            const status = getOrDefault(
+              cardStates,
+              card.name,
+              CardStateEnum.NOT_OBTAINED
+            );
             const isSelected = selectedCardState.includes(status);
             return (
               isSelected && (
